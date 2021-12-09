a0c3d432919b27ce728a9a3bbe88a2e6777d3175
HamzaNa1 <hamzamoayad2014@gmail.com>
2021-12-09 09:28:46 +0300

    Quality of Life Improvements

--- Portaler.NET/Shared/GameInfo/ZoneGenerator.cs
+++ Portaler.NET/Shared/GameInfo/ZoneGenerator.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Json;
 using System.Threading.Tasks;
@@ -7,11 +8,16 @@ namespace Portaler.NET.Shared.GameInfo
 {
     public static class ZoneGenerator
     {
-        public static ZoneInfo[]? Zones { get; set; }
+        public static ZoneInfo[]? Zones { get; private set; }
 
         public static async Task Initialize()
         {
-            Zones = await new HttpClient().GetFromJsonAsync<ZoneInfo[]>("https://raw.githubusercontent.com/HamzaNa1/data-dump/main/zones.json");
+            List<ZoneInfo>? zones = await new HttpClient().GetFromJsonAsync<List<ZoneInfo>>("https://raw.githubusercontent.com/HamzaNa1/data-dump/main/zones.json");
+            if (zones is not null)
+            {
+                zones.RemoveAll(x => x.AlbionId.Contains("---"));
+                Zones = zones.ToArray();
+            }
         }
 
         public static ZoneInfo? GetZone(string zoneName)
